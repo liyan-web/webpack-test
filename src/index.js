2308e99322442076341b7934c8dd18fd36d56317
yan <liyan.web@foxmail.com>
2020-09-27 19:16:10 +0800

    add babel-loader、core-js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import './index.css';
 
 const fn = () => {
   document.querySelector('#app').innerHTML = 'hello world';
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 3000);
+  });
 };
 
-fn();
+fn().then(() => {
+  document.querySelector('#app').innerHTML = 'changed';
+});
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,24 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3,
+                },
+              ],
+            ],
+          },
+        },
+      },
     ],
   },
   plugins: [
